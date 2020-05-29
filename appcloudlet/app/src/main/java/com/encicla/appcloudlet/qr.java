5249de02c15cea575bec99bbe40f521092038e07
machineblock <a.ruizg1450@gmail.com>
2020-05-29 13:38:49 -0500

    QR

--- appcloudlet/app/src/main/java/com/encicla/appcloudlet/qr.java
+++ appcloudlet/app/src/main/java/com/encicla/appcloudlet/qr.java
@@ -1,8 +1,46 @@
 package com.encicla.appcloudlet;
 
 import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 @SuppressLint("Registered")
-public class qr extends AppCompatActivity {
+public class qr extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mscanerview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr);
+    }
+
+
+    public void btnescanear(View v){
+        mscanerview= new ZXingScannerView(this);
+        setContentView(mscanerview);
+        mscanerview.setResultHandler(this);
+        mscanerview.startCamera();
+
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        Log.v("HandleResult",result.getText());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Resultados del scan");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog= builder.create();
+        alertDialog.show();
+
+        mscanerview.resumeCameraPreview(this);
+    }
 }
--- appcloudlet/app/src/main/java/com/encicla/appcloudlet/score.java
+++ appcloudlet/app/src/main/java/com/encicla/appcloudlet/score.java
@@ -1,8 +1,14 @@
 package com.encicla.appcloudlet;
 
 import android.annotation.SuppressLint;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 @SuppressLint("Registered")
 public class score extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score);
+    }
 }
