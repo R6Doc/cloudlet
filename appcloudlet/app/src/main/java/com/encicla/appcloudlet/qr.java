13c440042bcd48927ad1a35daae6548b266062d8
R6Doc <michaelgalindez274@gmail.com>
2020-05-29 23:01:58 -0500

    adding working version

--- appcloudlet/app/src/main/java/com/encicla/appcloudlet/qr.java
+++ appcloudlet/app/src/main/java/com/encicla/appcloudlet/qr.java
@@ -1,8 +1,30 @@
 package com.encicla.appcloudlet;
 
-import android.annotation.SuppressLint;
 
-import androidx.appcompat.app.AppCompatActivity;
-@SuppressLint("Registered")
-public class qr extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class qr extends Fragment {
+
+
+    public qr() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.qr, container, false);
+    }
+
 }
