b4a7c28c6e21b8e4b2b0b27a114aa23185045b64
machineblock <a.ruizg1450@gmail.com>
2020-06-01 12:43:08 -0500

    ahora lee codigo QR

--- appcloudlet/app/src/main/java/com/encicla/appcloudlet/qr.java
+++ appcloudlet/app/src/main/java/com/encicla/appcloudlet/qr.java
@@ -8,10 +8,11 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.zxing.Result;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
 public class qr extends Fragment {
 
 
@@ -23,8 +24,9 @@ public class qr extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.qr, container, false);
     }
+
 
 }
